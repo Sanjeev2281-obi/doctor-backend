be31976b4bfafebdbc60f2887b85b01e58aeb424
Sanjeev.s <sanjeevasas2281@gmail.com>
2025-10-31 11:55:57 +0530

    Connected Aiven MySQL successfully

--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  List<User> findByEmail(String email);
+   List<User> findByEmail(String email);
 
 
 }
