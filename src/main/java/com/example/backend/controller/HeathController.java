08fc7b75d9c0222d05b6453ead39266dbe3d1634
Sanjeev <sanjeevasas2281@gmail.com>
2026-02-22 00:04:37 +0530

    last dance

--- src/main/java/com/example/backend/controller/HeathController.java
+++ src/main/java/com/example/backend/controller/HeathController.java
@@ -1,6 +1,6 @@
 package com.example.backend.controller;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class HeathController {
     
